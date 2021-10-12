bug seems to finally be fixed. The signs were the last problem

kernels.py
@@ -425,13 +425,13 @@ class Diff_SE_kernel(Kernel):
             # This notation is only valid in iPython
             #T(m,k) = factorial(2*m)*2^(k-m)/(factorial(m-k)*factorial(2*k))
             # As an actual Python file I need to use:
-            T = lambda m, k : (-1)**(m+k)*factorial(2*m)*2**(k-m)/(factorial(m-k)*factorial(2*k))
+            T = lambda m, k : factorial(2*m)*2**(k-m)/(factorial(m-k)*factorial(2*k))
         # odd
         # T(2*m+1, k) = (-1)^(m+k)*(2*m+1)!*2^(k-m)/((m-k)!*(2*k+1)!), k = 0..m. (End)
         else:
             # See above
             #T(m,k) = factorial(2*m+1)*2^(k-m)/(factorial(m-k)*factorial(2*k+1))
-            T = lambda m, k: (-1)**(m+k)*factorial(2*m+1)*2**(k-m)/(factorial(m-k)*factorial(2*k+1))
+            T = lambda m, k: factorial(2*m+1)*2**(k-m)/(factorial(m-k)*factorial(2*k+1))
 
         return [int(T(real_n, k)) for k in range(real_n+1)]
 
@@ -455,9 +455,10 @@ class Diff_SE_kernel(Kernel):
             degr_x2 = term[2]
             poly_coeffs = term[0]
             sign = self.asym_sign_matr[int(degr_x1)%int(4)][int(degr_x2)%int(4)]
+            print(f"SIGN: {sign}")
             K_0_exponents = [int(i*2) if int(degr_x1+degr_x2)%2 == 0 else int(i*int(2)+int(1)) for i in range(int((degr_x1+degr_x2)/2)+int(1))]
             coefficients = self.coeffs(int(degr_x1+degr_x2))
-            coefficients = [c*sign for c in coefficients]
+            coefficients = [c*sign*(-1)**i for i, c in enumerate(coefficients)]
             #coefficients = [coefficients[i]*int(-1)**i for i in range(int((degr_x1+degr_x2)/2)+int(1))]
             l_exponents = [np.ceil((degr_x1+degr_x2)/int(2)) + i for i in range(int((degr_x1+degr_x2)/2)+int(1))]
             derived_form_list.append([[poly_coeffs, coeff, int(l_exp), exp] for exp, coeff, l_exp in zip_longest(K_0_exponents, coefficients, l_exponents, fillvalue=0)])