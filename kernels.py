2cce05e320e5e16cee39a1a65f67c8c1d5bd3544
Andreas Besginow <andreas.besginow@th-owl.de>
2024-04-08 13:11:39 +0200

    whatever changes I made to the kernel

--- kernels.py
+++ kernels.py
@@ -231,6 +231,8 @@ def translate_kernel_matrix_to_gpytorch_kernel(kernelmatrix, paramdict, common_t
             # Now translate the cell to a call
             replaced_op_cell = replace_basic_operations(str(cell))
             replaced_var_cell = replace_parameters(replaced_op_cell, paramdict, common_terms)
+            #print("DEBUG: replaced_var_cell:")
+            #print(replaced_var_cell)
             kernel_call_matrix[rownum].append(compile(replaced_var_cell, "", "eval"))
 
 
