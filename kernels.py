bfc6ca6fe202ee0263ffb00dd88ba9646836c347
Andreas Besginow <andreas.besginow@th-owl.de>
2025-05-15 10:48:23 +0200

    found a hack that enables us to run unsymmetric diffs of matern

--- kernels.py
+++ kernels.py
@@ -70,9 +70,17 @@ class LODE_Kernel(Kernel):
 def create_kernel_matrix_from_diagonal(D, **kwargs):
     base_kernel = kwargs["base_kernel"] if "base_kernel" in kwargs else "SE_kernel"
     if base_kernel == "Matern_kernel":
+        # Using numerical value because the regex is not yet properly implemented
         sqrt_3 = sqrt(3).n()
         # TODO abs around t1-t2
-        base_kernel_expression = lambda i : globals()[f"signal_variance_{i}"]**2 * (1 + sqrt_3*(t1 - t2)/globals()[f"lengthscale_{i}"])*exp(-sqrt_3*(t1 - t2)/globals()[f"lengthscale_{i}"])
+        #base_kernel_expression = lambda i : globals()[f"signal_variance_{i}"]**2 * (1 + sqrt_3*(t1 - t2)/globals()[f"lengthscale_{i}"])*exp(-sqrt_3*(t1 - t2)/globals()[f"lengthscale_{i}"])
+    elif base_kernel == "Matern_kernel_52":
+        sqrt_5 = sqrt(5).n()
+        # TODO set const
+        # TODO make sqrt regex-able
+        # TODO set base_kernel_expression to mat52 expression
+        #mat52 = (1 + sqrt_5*(sqrt((t1 - t2+const)**2)) + (5*(sqrt((t1 - t2+const)**2))**2)/3)*exp(-sqrt_5*(sqrt((t1 - t2+const)**2)))
+        base_kernel_expression = lambda i : globals()[f"signal_variance_{i}"]**2 * (1 + sqrt_5*(t1 - t2)/globals()[f"lengthscale_{i}"] + 5*(t1-t2)/(3*globals()[f"lengthscale_{i}"]**2))*exp(-sqrt_5*(t1 - t2)/globals()[f"lengthscale_{i}"])
     elif base_kernel == "SE_kernel":
         base_kernel_expression = lambda i : globals()[f"signal_variance_{i}"]**2 * exp(-1/2*(t1-t2)**2/globals()[f"lengthscale_{i}"]**2)
     t1, t2 = var("t1, t2")
@@ -187,6 +195,7 @@ def replace_basic_operations(kernel_string):
     # Define the regex replacement rules for the text
     regex_replacements_multi_group = {
         "exp" : [r'(e\^)\((([^()]*|\(([^()]*|\([^()]*\))*\))*)\)', "torch.exp"],
+        # TODO fix this
         "sqrt" : [r'sqrt(\((([^()]*|\(([^()]*|\([^()]*\))*\))*)\))', "torch.sqrt"],
         "exp_singular" : [r'(e\^)([0-9a-zA-Z_]*)', "torch.exp"]
     }
